62e4ade2938b5a5cf1c1c5079f7bf2831e650983
sagar <saliansagar4@gmail.com>
2017-06-11 16:17:41 +0530

    masonry effect for gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MasonryModule } from 'angular2-masonry';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -25,6 +26,7 @@ import { AboutComponent } from './about/about.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    MasonryModule,
     routing
   ],
   providers: [GalleryService],
